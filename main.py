dda3a3d2c849ced18b40871105f69ed92497497c
Robinzon100 <robinzon.sokhashvili@gmail.com>
2020-06-17 01:40:04 +0400

    :star: initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# Beat tracking example
+from __future__ import print_function
+import librosa
+import sys
+
+# 1. Get the file path to the included audio example
+filename = librosa.util.example_audio_file()
+
+# 2. Load the audio as a waveform `y`
+#    Store the sampling rate as `sr`
+y, sr = librosa.load(sys.argv[1])
+
+# 3. Run the default beat tracker
+tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
+
+print('Estimated tempo: {:.2f} beats per minute'.format(tempo))
+
+
+
+
